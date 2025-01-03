503a6e6d1f867a1377776bd1ebe5c46285b7d8c9
Waxmatuct <alvheim@rambler.ru>
2025-01-03 12:29:35 +0300

    свитчер темы

--- assets/js/switcher.js
+++ assets/js/switcher.js
@@ -1,20 +1,19 @@
 export default function switcher() {
     Alpine.store("darkMode", {
         init() {
-            // check for active theme
             let htmlElement = document.documentElement;
             let theme = localStorage.getItem("theme");
             let htmlClass = htmlElement.classList;
-            if (theme === "dark") {
-                htmlElement.setAttribute("theme", "dark");
+            if (
+                theme === "dark" ||
+                (!("theme" in localStorage) &&
+                    window.matchMedia("(prefers-color-scheme: dark)").matches)
+            ) {
                 htmlClass.add("dark");
                 this.on = true;
-                console.log("dark");
             } else {
-                htmlElement.setAttribute("theme", "light");
                 htmlClass.remove("dark");
                 this.on = false;
-                console.log("light");
             }
         },
         toggle() {
@@ -24,11 +23,9 @@ export default function switcher() {
             if (this.on) {
                 localStorage.setItem("theme", "dark");
                 htmlClass.add("dark");
-                htmlElement.setAttribute("theme", "dark");
             } else {
                 localStorage.setItem("theme", "light");
                 htmlClass.remove("dark");
-                htmlElement.setAttribute("theme", "light");
             }
         },
     });
